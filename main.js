de1d561fbf8c570051adf918e7dc591d9fdd6283
madhusudhan1234 <madhu@acd.edu.np>
2016-09-30 21:09:10 +0545

    Adding CRUD using Bootstrap modal

---
+++ main.js
@@ -0,0 +1,33 @@
+Vue.component('tasks',{
+    template: '#tasks-template',
+
+    props: ['list'],
+
+    data: function () {
+       return{
+           list: []
+       }
+    },
+
+    created: function () {
+        this.fetchTasks();
+    },
+    
+    methods : {
+        fetchTasks: function(){
+            this.$http.get('api/tasks',function (tasks) {
+                console.log(tasks);
+                 this.list = tasks;
+            }.bind(this));
+        },
+
+        deleteTask: function (task) {
+            this.list.$remove(task);
+        }
+    }
+});
+
+new Vue({
+    el: 'body'
+});
+//# sourceMappingURL=main.js.map
---
+++ resources/assets/js/pages.js
@@ -0,0 +1,11 @@
+import Vue from 'vue';
+
+import HomeView from './components/HomeView.vue';
+
+new Vue({
+    el: 'body',
+
+    components:{
+        HomeView
+    }
+});
